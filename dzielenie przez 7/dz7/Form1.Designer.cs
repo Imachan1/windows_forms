1689a2e3ba740483ea8fde4d537b7792e9aeeb30
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-03-11 22:31:31 +0100

    Add files via upload

---
+++ dzielenie przez 7/dz7/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace dz7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputField = new System.Windows.Forms.TextBox();
+            this.confirmButton = new System.Windows.Forms.Button();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputField
+            // 
+            this.inputField.Location = new System.Drawing.Point(121, 74);
+            this.inputField.Name = "inputField";
+            this.inputField.Size = new System.Drawing.Size(100, 22);
+            this.inputField.TabIndex = 0;
+            this.inputField.TextChanged += new System.EventHandler(this.inputField_TextChanged);
+            // 
+            // confirmButton
+            // 
+            this.confirmButton.Location = new System.Drawing.Point(121, 126);
+            this.confirmButton.Name = "confirmButton";
+            this.confirmButton.Size = new System.Drawing.Size(75, 23);
+            this.confirmButton.TabIndex = 1;
+            this.confirmButton.Text = "button1";
+            this.confirmButton.UseVisualStyleBackColor = true;
+            this.confirmButton.Click += new System.EventHandler(this.confirmButton_Click);
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.Location = new System.Drawing.Point(121, 166);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(44, 16);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "label1";
+            this.outputLabel.Click += new System.EventHandler(this.outputLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.confirmButton);
+            this.Controls.Add(this.inputField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputField;
+        private System.Windows.Forms.Button confirmButton;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ dzielenie przez 7/dz7/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dz7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void confirmButton_Click(object sender, EventArgs e)
+        {
+            string input = inputField.Text;
+
+            if (int.TryParse(input, out int number))
+            {
+                if (number % 7 == 0)
+                {
+                    outputLabel.Text = "tak";
+                }
+                else
+                {
+                    outputLabel.Text = "nie";
+                }
+            }
+            else
+            {
+                outputLabel.Text = "wpisz liczbe!";
+            }
+        }
+
+        private void inputField_TextChanged(object sender, EventArgs e)
+        {
+            outputLabel.Text = "";
+        }
+        private void outputLabel_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("nie kliknieto");
+        }
+
+    }
+
+}
+

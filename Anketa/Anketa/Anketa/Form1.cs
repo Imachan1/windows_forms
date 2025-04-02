7f16b60d2d6978ecda8cadf564319b2d719077e2
Valeriia <139450358+Imachan1@users.noreply.github.com>
2025-04-02 13:17:03 +0200

    Add files via upload
    Anketa cz. 1

---
+++ Anketa/Anketa/Anketa/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Anketa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(textBox1.Text+" "+ textBox2.Text);
+            MessageBox.Show(textBox1.Text + " " + textBox2.Text+" "+checkedListBox1.Text+" "+comboBox1.Text);
+        }
+    }
+}
